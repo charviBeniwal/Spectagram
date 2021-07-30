6e57f1e0e85b9a3c30d805f8acf1a9defddc6ef8
charviBeniwal <63788560+charviBeniwal@users.noreply.github.com>
2021-07-30 10:28:46 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,33 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/Home';
-import StarMapScreen from './screens/StarMap';
-import DailyPicScreen from './screens/DailyPic';
-import SpaceCraftsScreen from './screens/SpaceCrafts';
-import 'react-native-gesture-handler';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
+import DrawerNavigator from "./navigation/DrawerNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="StarMap" component={StarMapScreen} />
-        <Stack.Screen name="DailyPic" component={DailyPicScreen} />
-        <Stack.Screen name="SpaceCrafts" component={SpaceCraftsScreen} />
-      </Stack.Navigator> </NavigationContainer>
-
+      <DrawerNavigator/>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
