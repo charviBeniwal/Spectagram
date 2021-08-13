5e31a344bfdc8cfc51bda24ae6e6e2543315edc4
charviBeniwal <63788560+charviBeniwal@users.noreply.github.com>
2021-08-13 15:10:33 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import DrawerNavigator from "./navigation/DrawerNavigator";
+import * as React from "react";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <DrawerNavigator/>
-    </NavigationContainer>
-  );
+import LoginScreen from "./screens/LoginScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+//import DashboardScreen from "./screens/DashboardScreen";
+
+import firebase from "firebase";
+import { firebaseConfig } from "./config";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
 }
 
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+ // DashboardScreen: DashboardScreen
+});
 
+const AppNavigator = createAppContainer(AppSwitchNavigator);
+
+export default function App() {
+  return <AppNavigator />;
+}
---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA1YqvSA1fBb5QT5ENHS061TrlRHJcY7Vk",
+    authDomain: "spectagram-e249d.firebaseapp.com",
+    projectId: "spectagram-e249d",
+    storageBucket: "spectagram-e249d.appspot.com",
+    messagingSenderId: "308746120313",
+    appId: "1:308746120313:web:5558ac50602b7361dc38a8"
+  };
